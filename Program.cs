b7427d962e661af9ffcf9404d2c015175edbf731
Irina <89231707647@mail.ru>
2022-03-03 13:33:57 +0700

    3 Задача про четное и нечетное

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите чсло =>");  // вывод пргилащшеня для ввода
+string strValue = Console.ReadLine();   // ввод строки
+
+int value = int.Parse(strValue);        // преобразуем строку в целое число
+
+            if (value % 2 == 0)
+            {
+                Console.WriteLine("четное");
+            }
+            else
+            {
+                Console.WriteLine("не четное");
+            }
